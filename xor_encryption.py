0b6be4ef3bfe9af564ea9528a09dfcf962b72cd3
antipixel1 <75756211+antipixel1@users.noreply.github.com>
2023-01-25 23:29:25 -0600

    Created function to XOR encrypt a file.

--- xor_encryption.py
+++ xor_encryption.py
@@ -10,17 +10,23 @@ def generate_random_key(length):
 
 #Function to XOR encrypt a string with a randomly generated key
 def random_xor_encrypt(data):
-    data_bytes = bytes(data, 'utf-8')
-    data_bits = ""
-    for element in data_bytes:
-        converted_element = str(bin(element))[2:]
-        while (len(converted_element)<8):
-            converted_element = "0" + converted_element
-        data_bits += converted_element
-    key = generate_random_key(len(data_bits))
-    encrypted = bin(int(data_bits,2) ^ int(key,2))
-    encrypted = str(encrypted)[2:]
-    return [encrypted, key]
+    encrypted_data = []
+    if len(data)>0:
+        data_bytes = bytes(data, 'utf-8')
+        data_bits = ""
+        for element in data_bytes:
+            converted_element = str(bin(element))[2:]
+            while (len(converted_element)<8):
+                converted_element = "0" + converted_element
+            data_bits += converted_element
+        key = generate_random_key(len(data_bits))
+        encrypted = bin(int(data_bits,2) ^ int(key,2))
+        encrypted = str(encrypted)[2:]
+        encrypted_data = [encrypted, key]
+    else:
+        print("Can't encrypt empty string.")
+    return encrypted_data
+
 
 #Function to convert binary strings to ascii
 def bin_to_ascii(data_bin):
@@ -46,7 +52,35 @@ def random_xor_decrypt(data_bits, key):
         decrypted = "0" + decrypted
     decrypted_ascii = bin_to_ascii(decrypted)
     return decrypted_ascii
+#Function to XOR encrypt a string by a number of parts, each with a randomly generated key
+def encrypt_parts(data, number_parts):
+    encrypted_parts = []
+    unit_size = len(data)//number_parts
+    if unit_size >=1:
+        for i in range (number_parts):
+            current_part = data[unit_size*i:unit_size*(i+1)]
+            if (i==number_parts-1):
+                current_part += data[unit_size*(i+1):len(data)]
+            current_part_encrypted = random_xor_encrypt(current_part)
+            encrypted_parts.append(current_part_encrypted)
+    else:
+        print("Can't create the number of parts selected")
+    return encrypted_parts
 
+def encrypt_file(filename):
+    original_file = open(filename)
+    file_encrypted = open(filename+"_encrypted.txt", "x", encoding='utf-8')
+    encryption_keys_file = open(filename+"_encryption_keys.txt", "x", encoding='utf-8')
+    if (original_file):
+        for line in original_file:
+            encrypted_line = random_xor_encrypt(line)
+            file_encrypted.write(bin_to_ascii(encrypted_line[0])+"\n")
+            encryption_keys_file.write(bin_to_ascii(encrypted_line[1])+"\n")
+        original_file.close()
+    else:
+        print("Error reading the file")
+    file_encrypted.close()
+    encryption_keys_file.close()
 #Driver code
 def __main__():
     sys.stdout.reconfigure(encoding='utf-8')
@@ -61,6 +95,8 @@ def __main__():
     print("Encrypted string in ascii: ", data_ascii)
     original_string = random_xor_decrypt(encrypted_string[0], encrypted_string[1])
     print("Original string:", original_string)
-    
+    encrypted_parts = encrypt_parts("Hola que hace xddd", 5)
+    print(encrypted_parts)
+    encrypt_file("test.txt")
 if __name__ == "__main__":
     __main__()
